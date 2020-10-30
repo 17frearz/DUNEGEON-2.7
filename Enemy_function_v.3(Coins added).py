c453d226daef5dc0cff1acfc1e1d313fd142cd26
17frearz <69487951+17frearz@users.noreply.github.com>
2020-10-30 14:15:06 +1300

    Add files via upload

---
+++ Enemy_function_v.3(Coins added).py
@@ -0,0 +1,25 @@
+"""
+    Author: Zeph Frear Hanson
+    Date: 30-10-2020
+    Title: Coin List
+    Description: list for the coins stored
+    Improvements(Use if altering the code): 
+"""
+
+#==-Libraries-==#
+
+#====-Functions-====#
+def list():
+    coin = 47
+    print(users[0],users[1])
+    print(users[0],int(users[1])+coin)
+    
+    
+
+#====-Main Routine-====#
+
+users = ['Player 1','30',
+         'Player 2','14',
+         'Player 3','45']
+
+list()
